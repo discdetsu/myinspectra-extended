30fc84b64ee7c8e03d4dd8089854d079e104782a
discdetsu <disk.nitrome@gmail.com>
2025-04-18 12:58:30 +0700

    Create db model prediction

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Prediction
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
   list_display = ("firstname", "lastname", "joined_date",)
+
+class PredictionAdmin(admin.ModelAdmin):
+  list_display = ("user", "created_at",)
   
 admin.site.register(User, UserAdmin)
+admin.site.register(Prediction)
--- backend/api/models.py
+++ backend/api/models.py
@@ -6,3 +6,9 @@ class User(models.Model):
     lastname = models.CharField(max_length=255)
     phone = models.IntegerField(null=True)
     joined_date = models.DateTimeField(auto_now_add=True, null=True)
+
+class Prediction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
+    result = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
