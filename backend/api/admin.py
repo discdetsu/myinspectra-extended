5e392c600aa26babc63f2f649ee41be832444168
discdetsu <disk.nitrome@gmail.com>
2025-05-09 14:50:47 +0700

    Apply admin page for Report model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Prediction
+from .models import User, Prediction, Report
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
@@ -7,6 +7,10 @@ class UserAdmin(admin.ModelAdmin):
 
 class PredictionAdmin(admin.ModelAdmin):
   list_display = ("user", "created_at",)
+
+class ReportAdmin(admin.ModelAdmin):
+  list_display = ("prediction", "created_at", "pdf")
   
 admin.site.register(User, UserAdmin)
-admin.site.register(Prediction)
+admin.site.register(Prediction, PredictionAdmin)
+admin.site.register(Report, ReportAdmin)
